96477cc25d5ebe42696c9074f1f600fcac84ef5d
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-06-29 14:16:28 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,32 +7,34 @@ android {
     compileSdk = 34
 
     defaultConfig {
-    applicationId = "com.nemesis.mocktraffic"
-    minSdk = 24
-    targetSdk = 34
-    versionCode = 2
-    versionName = "1.1"
+        applicationId = "com.nemesis.mocktraffic"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.1"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-    dependenciesInfo {
-        includeInApk = false
-        includeInBundle = false
+        dependenciesInfo {
+            includeInApk = false
+            includeInBundle = false
+        }
     }
-}
 
-buildTypes {
-    release {
-        isMinifyEnabled = true
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro"
-        )
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
-}
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
@@ -42,6 +44,7 @@ dependencies {
     implementation("org.json:json:20230227")
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.10.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
